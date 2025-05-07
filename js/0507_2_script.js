09a2d8dca65a2f48c8e328a5be11f4b316269018
alswn8477 <alswn8477@naver.com>
2025-05-07 18:02:54 +0900

    first commit

---
+++ js/0507_2_script.js
@@ -0,0 +1,16 @@
+// script.js
+function loadAlert() {
+  alert("Test!");
+}
+
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    navigation: true,
+    navigationPosition: "right",
+  });
+});
+
+// section0
+{
+  /* <script>AOS.init( duration: 1000, delay: 500,);</script>; */
+}
---
+++ js/0507_script.js
@@ -0,0 +1,11 @@
+// script.js
+function loadAlert() {
+  alert("Test!");
+}
+
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    navigation: true,
+    navigationPosition: "right",
+  });
+});
