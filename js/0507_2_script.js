8d71fe35d382d7bc84808c5fd01d89a6e7c3bd88
alswn8477 <alswn8477@naver.com>
2025-05-07 19:53:29 +0900

    first commit

--- js/0507_2_script.js
+++ js/0507_2_script.js
@@ -3,12 +3,82 @@ function loadAlert() {
   alert("Test!");
 }
 
+// fullpage
 $(document).ready(function () {
   $("#fullpage").fullpage({
     navigation: true,
     navigationPosition: "right",
   });
 });
+
+// 꽃 날림
+$(function () {});
+const canvas = document.getElementById("leafCanvas");
+const ctx = canvas.getContext("2d");
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+const leafImage = new Image();
+leafImage.src = "./images/꽃2.png"; // (원하는 PNG로 바꾸세요)
+const leafCount = 20;
+const leaves = [];
+class Leaf {
+  constructor() {
+    this.reset();
+  }
+  reset() {
+    this.x = canvas.width + Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = 40 + Math.random() * 60;
+    this.speedX = 1 + Math.random() * 3;
+    this.angle = Math.random() * Math.PI * 2;
+    this.angleSpeed = (Math.random() - 0.5) * 0.05;
+    this.amplitude = 10 + Math.random() * 20;
+  }
+  update() {
+    this.x -= this.speedX;
+    this.y += Math.sin(this.angle) * 0.5;
+    this.angle += this.angleSpeed;
+    if (this.x < -this.size) {
+      this.reset();
+      this.x = canvas.width + this.size;
+    }
+  }
+  draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.drawImage(
+      leafImage,
+      -this.size / 2,
+      -this.size / 2,
+      this.size,
+      this.size
+    );
+    ctx.restore();
+  }
+}
+function init() {
+  for (let i = 0; i < leafCount; i++) {
+    leaves.push(new Leaf());
+  }
+}
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let leaf of leaves) {
+    leaf.update();
+    leaf.draw();
+  }
+  requestAnimationFrame(animate);
+}
+leafImage.onload = () => {
+  init();
+  animate();
+};
 
 // section0
 {
